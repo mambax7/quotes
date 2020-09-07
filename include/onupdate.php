e3d9ffc65a2411ef72a3b8318aeb5bd1d6be10a3
mambax7 <mambax7@gmail.com>
2020-09-07 03:03:36 -0400

    Low performing filesystem operations

--- include/onupdate.php
+++ include/onupdate.php
@@ -119,7 +119,7 @@ function xoops_module_update_quotes(\XoopsModule $module, $previousVersion = nul
                         foreach ($templateList as $k => $v) {
                             $fileInfo = new SplFileInfo($templateFolder . $v);
                             if ('html' === $fileInfo->getExtension() && 'index.html' !== $fileInfo->getFilename()) {
-                                if (file_exists($templateFolder . $v)) {
+                                if (is_file($templateFolder . $v)) {
                                     unlink($templateFolder . $v);
                                 }
                             }
